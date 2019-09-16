7228133b8b3299a2553dde8f7bd5b28bfb067d29
viral32111 <19510403+viral32111@users.noreply.github.com>
2019-09-16 18:04:08 +0100

    Added Libgcrypt and a bunch of other crap

--- source/gm_hash.cpp
+++ source/gm_hash.cpp
@@ -1,11 +1,31 @@
-#include "GarrysMod/Lua/Interface.h"
 #include <stdio.h>
+#include <string>
+using namespace std;
 
+// Garry's Mod
+#include "GarrysMod/Lua/Interface.h"
 using namespace GarrysMod::Lua;
 
-LUA_FUNCTION(SHA256) { // SHA256 is the name of the function
+// Crypto++
+using namespace CryptoPP;
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/sha.h"
+#include "cryptopp/filters.h"
+#include "cryptopp/base64.h"
+
+string hashString(string aString) {
+	string digest;
+	CryptoPP::SHA256 hash;
+
+	CryptoPP::StringSource foo(aString, true, new CryptoPP::HashFilter(hash, new CryptoPP::Base64Encoder(new CryptoPP::StringSink(digest))));
+
+	return digest;
+}
+
+LUA_FUNCTION(sha256Function) { // SHA256 is the name of the function
     if (LUA->IsType(1, Type::STRING)) { // If parameter one is a string
-        LUA->PushString(LUA->GetString(1)); // Return what they passed
+		//const char* shaHash = SHA256HashString(LUA->GetString(1)).c_str(); // Generate SHA256 Hash
+        //LUA->PushString(shaHash); // Return that hash
         return 1; // End
 	} else {
 		LUA->PushNil(); // Return nil if no string parameter is provided
@@ -17,7 +37,7 @@ LUA_FUNCTION(SHA256) { // SHA256 is the name of the function
 GMOD_MODULE_OPEN() {
     LUA->PushSpecial(SPECIAL_GLOB); // Push global table
     LUA->PushString("SHA256"); // Push Name
-    LUA->PushCFunction(SHA256); // Push function
+    LUA->PushCFunction(sha256Function); // Push function
     LUA->SetTable(-3); // Set the table 
 
     return 0;
